5f0ba0ed2060c5b64c4d877108e6f41164b5d8a8
Ryan Rivard <rivard0512@gmail.com>
2025-08-16 17:16:59 -0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ if uploaded_file is not None:
         if not os.path.exists(model_path):
             file_id = "1WgchUqXf1mrLJ8pl3l0qwgRuwcCgi2S_"
             url = f"https://drive.google.com/uc?id=1WgchUqXf1mrLJ8pl3l0qwgRuwcCgi2S_" 
-            gdown.download(url, model_path, quiet=False)
+            gdown.download(url, "yolov5m-fp16.tflite", quiet=False)
 
         # Run detection - now also gets cropped images
         detections, output_img_path, ai_results, cropped_images = boundingboxes.run_detection(temp_path, model_path)
